3a752eddc570423a23d45186936f2afdcec822b5
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-06 19:39:45 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function quicksort(array) {
+    var len = array.length, pivot = 0;
+    pivot = sort(array, stop, len);
+    while (pivot != 99)
+        pivot = sort(array, pivot + 1, len);
+        pivot = sort(array, 0, pivot - 1);
     return array;
+}
+
+function sort(array, low, high) {
+    var iter, pivot;
+    if (lo >= hi) return 99;
+    p = lo;
+    for (i = lo +1; i <= hi; i++)
+        if (array[iter] < array[lo])
+            var swap = array[++p];
+            array[p] = array[i];
+            array[i] = swap;
+    swap = array[low];
+    array[low] = array[p];
+    array[p] = swap;
+    return p;
 }
